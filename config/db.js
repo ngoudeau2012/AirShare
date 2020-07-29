018b882f46afe418d560881ae546c0d62cfa5ca8
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-07-28 21:42:32 -0700

    basic server install with db connection and middleware

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const db = process.env.URI;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const expressSession = require("express-session");
+const db = require("./config/db");
+
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+// MW
+const app = express();
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(logger("combined"));
+app.use(
+  expressSession({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
+// DB
+db();
+// DB => API
+app.use("/api/user", require("./routes/api/user"));
+
+app.listen(PORT, () => {
+  console.log(`PORT ${PORT}`);
+});
