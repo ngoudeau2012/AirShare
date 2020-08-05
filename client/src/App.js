bc9e376f5fc5135b0186d9b86e5b969f24e7c373
Nicholas Goudeau <ngoudeau2012@gmail.com>
2020-08-04 19:55:23 -0700

    fixed remaining merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,6 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/HomePage/HomePage'
-<<<<<<< HEAD
-import ContactPage from './pages/ContactPage/ContactPage'
-import MeetTeam from './pages/MeetTeam/MeetTeam'
-
-function App() {
-  return (
-    <div className="App">
-
-     <HomePage />
-     <ContactPage />
-     <MeetTeam />
-    </div>
-=======
 import MeetTeam from './pages/MeetTeam/MeetTeam'
 import ContactPage from './pages/ContactPage/ContactPage'
 import Signup from './pages/SignUp/Signup'
@@ -29,7 +16,6 @@ function App() {
       <Route exact path = "/sign-up" component={Signup} />
       <Route exact path = "/login" component={Login} />
     </Router>
->>>>>>> browser-router
   );
 }
 
--- client/src/components/HomeNav/HomeNav.js
+++ client/src/components/HomeNav/HomeNav.js
@@ -1,28 +1,13 @@
 import React from "react";
 import "./HomeNav.css";
 import { Link } from "react-router-dom";
+import ContactPage from "../../pages/ContactPage/ContactPage";
 
 function HomeNav() {
-<<<<<<< HEAD
-
-    return (
-        <div>
-          <div></div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" Link to="/home">AirShare</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div className="navbar-nav">
-      <a className="nav-item nav-link" href="#">Log In</a>
-      <a className="nav-item nav-link" Link to="/meetteam">Meet the Team</a>
-=======
   return (
     <div>
-      <div></div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand" Link to="/home">
           AirShare
         </a>
         <button
@@ -41,13 +26,12 @@ function HomeNav() {
             <a className="nav-item nav-link" href="#">
               Log In
             </a>
-            <a className="nav-item nav-link" href="#">
+            <a className="nav-item nav-link" Link to="/meetteam">
               Meet the Team
             </a>
           </div>
         </div>
       </nav>
->>>>>>> browser-router
     </div>
   );
 }
