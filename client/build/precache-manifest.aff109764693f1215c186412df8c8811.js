bc6078a618e8e2c1c963ad2e9cbd8ace09151f00
Nicholas Goudeau <ngoudeau2012@gmail.com>
2020-08-08 15:19:38 -0700

    Deploying to heroku

---
+++ client/build/precache-manifest.aff109764693f1215c186412df8c8811.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1385ecde1d2a0354e1bb",
+    "url": "/static/css/main.25902d4f.chunk.css"
+  },
+  {
+    "revision": "1385ecde1d2a0354e1bb",
+    "url": "/static/js/main.fe5c25c0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e178da2fec53e62b40ab",
+    "url": "/static/js/2.1c93b243.chunk.js"
+  },
+  {
+    "revision": "bcd272039af6e0f383f701b594f917a0",
+    "url": "/index.html"
+  }
+];
