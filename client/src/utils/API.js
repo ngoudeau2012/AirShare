ca57412613bbdd1f10729c3535d1e302c0efcee5
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-08-08 10:57:30 -0700

    api connection

---
+++ client/src/utils/API.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+export default {
+  newUser: (data) => {
+    return axios.post("/api/user/create", data);
+  },
+  loginUser: (data) => {
+    return axios.post("/api/user/create", data);
+  },
+  deleteUser: (data) => {
+    return axios.delete("/api/user/delete", data);
+  },
+  getAllUsers: () => {
+    return axios.get("/api/user/getall");
+  },
+  updateCredentials: (data) => {
+    return axios.put("/api/user/update", data);
+  },
+  updateCard: (params, data) => {
+    return axios.post("/api/accounts/" + params + "/information", data);
+  },
+  getAllUserInfo: (params) => {
+    return axios.get("/api/accounts/" + params + "/information");
+  },
+  getAllUserContacts: (params) => {
+    return axios.get("/api/accounts/" + params + "/contacts");
+  },
+  getAll: (params) => {
+    return axios.get("/api/accounts/" + params);
+  },
+  addContact: (params, data) => {
+    return axios.put("/api/accounts/" + params + "/contacts/add", data);
+  },
+  deleteContact: (params, data) => {
+    return axios.put("/api/accounts/" + params + "/contacts/add", data);
+  },
+};
