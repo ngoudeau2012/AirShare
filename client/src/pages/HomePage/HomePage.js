c0f352390cfbb1243c009d08ff812d33cc537d43
Christina Ausley <christinamausley@gmail.com>
2020-08-07 20:26:56 -0700

    added steps to homepage

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -8,6 +8,8 @@ import Container from "react-bootstrap/Container";
 import QuoteCard from "../../components/QuoteCard/QuoteCard";
 import Card from "../../components/Card/Card";
 import aboutus from "./aboutus.json";
+import StepCard from "../../components/StepCard/StepCard";
+import step from "./step.json";
 import Footer from "../../components/Footer/Footer";
 import "../../App.css"
 
@@ -44,6 +46,50 @@ function HomePage() {
               image={aboutus[2].image}
               idescription={aboutus[2].idescription}
               cardtext={aboutus[2].cardtext}
+            />
+          </Col>
+        </Row>
+
+      </Container>
+
+      <div style={{color: "white"}}>
+          <br></br>
+          <h1 style={{border: "10px", borderStyle: "groove", borderColor: "white"}}>How does AirShare work?</h1>
+          <br></br>
+        </div>
+
+        <Container>
+        <Row>
+          <Col>
+            <StepCard 
+              stepimage={step[0].stepimage}
+              stepimagedescription={step[0].stepimagedescription}
+              stepcardtitle={step[0].stepcardtitle}
+              stepcarddescription={step[0].stepcarddescription}
+            />
+          </Col>
+          <Col>
+          <StepCard 
+              stepimage={step[1].stepimage}
+              stepimagedescription={step[1].stepimagedescription}
+              stepcardtitle={step[1].stepcardtitle}
+              stepcarddescription={step[1].stepcarddescription}
+            />
+          </Col>
+          <Col>
+          <StepCard 
+              stepimage={step[2].stepimage}
+              stepimagedescription={step[2].stepimagedescription}
+              stepcardtitle={step[2].stepcardtitle}
+              stepcarddescription={step[2].stepcarddescription}
+            />
+          </Col>
+          <Col>
+          <StepCard 
+              stepimage={step[3].stepimage}
+              stepimagedescription={step[3].stepimagedescription}
+              stepcardtitle={step[3].stepcardtitle}
+              stepcarddescription={step[3].stepcarddescription}
             />
           </Col>
         </Row>
