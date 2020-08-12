8cfbe8d3121596c204044b20afcf896559ab9108
Christina Ausley <christinamausley@gmail.com>
2020-08-11 18:59:43 -0700

    updated length of card

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -4,7 +4,7 @@ import "./Card.css";
 function Card (props) {
   return (
     <div className="animate__animated animate__fadeInUp">
-    <div className="card" style={{minHeight: "380px", marginBottom: "30px", border: "20px solid #1b262c"}}>
+    <div className="card" style={{minHeight: "450px", marginBottom: "30px", border: "20px solid #1b262c"}}>
     <img src={props.image} style={{maxHeight: "250px"}} className="card-img-top" alt={props.idescription}/>
     <div className="card-body">
         <p className="card-text">{props.cardtext}</p>
