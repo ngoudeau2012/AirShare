a6223dc935622f2e9b8d153d258d195f30d171d2
Dylan Bremner <61300825+bremnerdc@users.noreply.github.com>
2020-08-11 21:15:02 -0700

    added some boilerplate to profile page

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Profile.css";
 import Navbar from "../../components/Navbar/Navbar"
 import ECard from "../../components/ECard/ECard";
+import ProfileCard from "../../components/ProfileCard/index"
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
@@ -24,17 +25,21 @@ function UserProfile() {
 
         <div style={{color: "white"}}>
           <br></br>
-          <h1 style={{border: "10px", borderStyle: "groove", borderColor: "white"}}>Your Profile</h1>
+          <h1 style={{border: "10px", borderStyle: "groove", borderColor: "white"}}>Who's In Your Network...</h1>
           <br></br>
         </div>
  
         <Container>
-        
-        </Container>
+            <Row>
+            {/* <ProfileCard 
+                    profile={formstate}
+                    /> */}
+                </Row>
+
+             </Container>
       
         <Footer/>
         </div>
-
     );
 }
 
