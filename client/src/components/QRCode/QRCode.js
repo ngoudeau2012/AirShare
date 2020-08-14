2a83f1ebc6c0cf11a34c0567fb2c476f9291c974
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-08-14 08:18:52 -0700

    dev/prod switch for qr code

--- client/src/components/QRCode/QRCode.js
+++ client/src/components/QRCode/QRCode.js
@@ -3,7 +3,9 @@ import "./QRCode.css";
 import QRCode from "qrcode.react";
 
 function QR(props) {
-  let val = `http://localhost:5000/api/accounts/${props.userID}`;
+  // let val = `http://localhost:5000/api/accounts/${props.userID}`;
+  let val = `https://airshare-generational.herokuapp.com/${props.userID}`;
+
   return <QRCode value={val} />;
 }
 
